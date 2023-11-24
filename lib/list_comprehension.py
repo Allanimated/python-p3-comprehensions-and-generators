7676c63d529e72f5c915417d97643f5baa5b399c
Allan Njoroge <allcreativewrites@gmail.com>
2023-11-24 12:46:58 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_integers = [i for i in num_list if i%2 == 0]
+    return (even_integers)
+
+#return_evens([1,2,3,4])
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation =[word + "!" for word in sentence_list]
+    return (add_exclamation)
+
+#make_exclamation(["hello", "world"])
+
